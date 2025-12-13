a33eb980af6010b9be7761f173d32af6c337bd27
Trent Crawford <trent.crawford@liip.ch>
2025-12-13 22:22:23 +0100

    Update application version to 1.3.0-alpha in Version class.

--- src/Version.php
+++ src/Version.php
@@ -9,7 +9,7 @@ namespace Tcrawf\Zebra;
  */
 final class Version
 {
-    public const string VERSION = '1.2.0-alpha';
+    public const string VERSION = '1.3.0-alpha';
 
     /**
      * Get the current application version.
